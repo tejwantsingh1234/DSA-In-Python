227edca7fadcd494a72a5c732cae3b04917ab621
tejwantsingh1234 <61786997+tejwantsingh1234@users.noreply.github.com>
2022-10-13 11:46:20 +0530

    Add files via upload

---
+++ Circular Linked List/Insertion at beg.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+struct node
+{
+    int data;
+    struct node *next;
+} *tail;
+
+void insertatbeg()
+{
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter data: ");
+    scanf("%d",&newnode->data);
+
+    if(tail==0)
+    {
+        tail = newnode;
+        tail->next = newnode;
+    }
+    else
+    {
+        newnode->next = tail->next;
+        tail->next = newnode;
+    }
+}
